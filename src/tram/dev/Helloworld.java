4daf93db7bc12e0744a8ef55b3cb50a500b50dfd
Tram <nykiu95@gmail.com>
2018-08-24 14:57:40 +0700

    Giaiphuongtrinhbac2

--- src/tram/dev/Helloworld.java
+++ src/tram/dev/Helloworld.java
@@ -2,7 +2,27 @@ package tram.dev;
 
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("Helloworld");
+		double a = 3;
+		double b = 2;
+		double c = 5;
+		double delta;
+		double x1, x2;
+		delta = (b*b) - (4*a*c);
+		if (delta < 0){
+			System.out.println("Phuong trinh vo nghiem");
+		}
+		
+		if (delta > 0){
+			x1 = (-b+Math.sqrt(delta)) / (2*a);
+			x2 = (-b-Math.sqrt(delta)) / (2*a);
+			System.out.println("x1 = " + x1);
+			System.out.println("x2 = " + x2);
+		}
+		
+		if (delta == 0) {
+			x1 = -b / (2*a);
+			System.out.println("Phuong trinh co nghiem kep x = " + x1);
+		}
 	}
 
 }
